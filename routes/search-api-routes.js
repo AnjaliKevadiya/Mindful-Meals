c8057255fe20dcd943fa5833cd62c5ad2b8aac42
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-09 14:39:29 -0500

    installed axios dependencies

--- routes/search-api-routes.js
+++ routes/search-api-routes.js
@@ -1,15 +1,20 @@
 // Requiring our models
 var db = require("../models");
+const axios = require("axios");
 
 // Routes
 // =============================================================
 module.exports = function (app) {
   app.get("/api/search", function (req, res) {
+    console.log(req.query.name);
     // axios({
     //   method: "get",
-    //   url: "http://bit.ly/2mTM3nY ${parm1} sfsfs ",
+    //   url: `https://api.edamam.com/api/food-database/v2/parser?ingr=${req.query.name}&app_id=67d00f61&app_key=d1f74c4f7cb95657ee6dd37b41502fb8`,
     // }).then(function (response) {
-    //   response.data.pipe(fs.createWriteStream("ada_lovelace.jpg"));
+    //   res.json({
+    //     text: response.data.text,
+    //     parsed: response.data.parsed,
+    //   });
     // });
   });
 };
