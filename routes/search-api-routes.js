305b80e38bcb78f32191dac1cf5ff1884b9127b2
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-09 14:40:19 -0500

    using axios npm call search api and send json response back

--- routes/search-api-routes.js
+++ routes/search-api-routes.js
@@ -7,14 +7,14 @@ const axios = require("axios");
 module.exports = function (app) {
   app.get("/api/search", function (req, res) {
     console.log(req.query.name);
-    // axios({
-    //   method: "get",
-    //   url: `https://api.edamam.com/api/food-database/v2/parser?ingr=${req.query.name}&app_id=67d00f61&app_key=d1f74c4f7cb95657ee6dd37b41502fb8`,
-    // }).then(function (response) {
-    //   res.json({
-    //     text: response.data.text,
-    //     parsed: response.data.parsed,
-    //   });
-    // });
+    axios({
+      method: "get",
+      url: `https://api.edamam.com/api/food-database/v2/parser?ingr=${req.query.name}&app_id=67d00f61&app_key=d1f74c4f7cb95657ee6dd37b41502fb8`,
+    }).then(function (response) {
+      res.json({
+        text: response.data.text,
+        parsed: response.data.parsed,
+      });
+    });
   });
 };
