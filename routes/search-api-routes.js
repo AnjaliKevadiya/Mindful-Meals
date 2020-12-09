fce2985e5efa61a5b10be8fe56fb95f561cabf79
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-09 13:49:29 -0500

    created search-api-routes.js and created endpoint for search api

---
+++ routes/search-api-routes.js
@@ -0,0 +1,15 @@
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+  app.get("/api/search", function (req, res) {
+    // axios({
+    //   method: "get",
+    //   url: "http://bit.ly/2mTM3nY ${parm1} sfsfs ",
+    // }).then(function (response) {
+    //   response.data.pipe(fs.createWriteStream("ada_lovelace.jpg"));
+    // });
+  });
+};
