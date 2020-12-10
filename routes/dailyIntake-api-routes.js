a8072cdd5efa001157fca1017e96e90c07fae286
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-10 15:39:31 -0500

    added data to database

--- routes/dailyIntake-api-routes.js
+++ routes/dailyIntake-api-routes.js
@@ -3,16 +3,12 @@ var db = require("../models");
 module.exports = function(app) {
     app.post("/api/addIntake", function (req, res) {
         console.log(req.body);
-        db.dailyIntake.create({
+        db.DailyIntake.create({
             name_of_food: req.body.name_of_food,
-            id: req.body.id
-        })
-        //logic for inserting data into dailyIntake
-        db.DailyIntake.create(req.body).then(function (dbDailyIntake) {
-            res.json(dbDailyIntake);
-        })
-        //add to dailyintake seperate query for this
-        //also for nutrient table
+            UserId: req.user.id
+        }).then( function (foodres) {
+            res.json(foodres)
+        });
     });
 
     app.get("/api/getIntake", function (req, res) {
