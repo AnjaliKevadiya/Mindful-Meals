e956d6a1f6c36a2c4b7a59925c45eaba4c6ffb9e
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-11 09:50:52 -0500

    when user click on profile screen check cuthentication if user is already logged in than only allow them to redirect to that page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,7 +27,8 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/profile", function (req, res) {
+  // route to redirect to profile page
+  app.get("/profile", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/profile.html"));
   });
 
