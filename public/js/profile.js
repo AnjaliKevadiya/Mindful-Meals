7d3616cf01f088d011242ceabfbd620719b71a2c
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-13 08:51:35 -0500

    get reference to form and inputs using variables

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,6 +4,23 @@ $(document).ready(function () {
         $(".member-name").text(data.email);
     });
 
-    
+    //getting reference to form and inputs
+    var updateBtn = $("#update");
+    var firstName = $("#firstName");
+    var lastName = $("#lastName");
+    var height = $("#height");
+    var weight = $("weight");
+    var age = $("#age");
+    var calorieIntake = $("#calorie-intake");
+    var activity = $("#activity");
+    var high = $("#high");
+    var active = $("#active");
+    var moderate = $("#moderate");
+    var low = ("#low");
+    var goal = ("#goal");
+    var lose = ("#lose");
+    var gain = ("#gain");
+    var maintain = ("maintian");
+
 
 });
