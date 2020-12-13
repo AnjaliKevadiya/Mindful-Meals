09aa92da96ec21f775f0d0aba21ab4a271b63a06
Ashvini Thomas <ashvinithomas@gmail.com>
2020-12-13 14:27:27 -0500

    take values from user input to put into database

--- public/js/profile.js
+++ public/js/profile.js
@@ -13,14 +13,26 @@ $(document).ready(function () {
     var age = $("#age");
     var calorieIntake = $("#calorie-intake");
     var activity = $("#activity");
-    var high = $("#high");
-    var active = $("#active");
-    var moderate = $("#moderate");
-    var low = ("#low");
     var goal = ("#goal");
-    var lose = ("#lose");
-    var gain = ("#gain");
-    var maintain = ("maintian");
+
+    updateBtn.on("click", function (event) {
+        event.preventDefault();
+        var updateData = {
+            first_name: firstName.val().trim(),
+            last_name: lastName.val().trim(),
+            age: age.val(),
+            height: Integer.parseInt(height),
+            weight: weight.val(),
+            no_of_active_days: activity.val(),
+            lose_or_gain_weight: goal.val()
+        }
+
+        console.log(updateData);
+
+        $.post("/api/user_info", updateData).then(function (result) {
+            console.log(result);
+        });
+    });
 
 
 });
